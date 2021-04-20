1dc8654665c7fa9b32b41104d4e5cd3b29b6fc50
Egoy Magnaye <webdev@egoymagnaye.com>
2021-04-21 06:39:00 +0800

    Add trailingslash next js configuration, fix main image in home page, fix the url of store locator

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9d5884e3a2db3f079d4d";
+/******/ 	var hotCurrentHash = "05bf1c9c702d2ac67da4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
