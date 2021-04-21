ac684eef5da09e1d257771d3ba5eb5d15e3c7587
Egoy Magnaye <webdev@egoymagnaye.com>
2021-04-21 21:19:21 +0800

    Fixing 404 issues. Updating next.js package.

--- _next/static/chunks/95b64a6e.bd862b3d5eef19fa1f48.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{IdFE:function(t,n,a){"use strict";a.d(n,"a",(function(){return h}));var c=a("Lnxd");function h(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M96.4 416h77.1l50.9-96.6V96h-160v223.4h77.1L96.4 416zm224 0h77.1l50-96.6V96H288.4v223.4h82l-50 96.6z"}}]})(t)}}}]);
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "46d9a0884d6212c661e5";
+/******/ 	var hotCurrentHash = "719c342e9547a9bb6223";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
